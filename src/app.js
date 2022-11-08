e904d4581048ea11fe1132b689986c8656d2aff3
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-08 23:02:01 +0100

    beginnetje gemaakt met data fetch vanuit lesvideo

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-console.log("Hallo daar!");
-console.log("dit moet meteen zichtbaar zijn");
+import axios
+    from "axios";
+
+const list = document.getElementById("country-list");
+
+async function getCountryData() {
+    const URI =" https://restcountries.com/";
+    const ENDPOINT = "v3.1/all" ;
+    try{
+        const response = await axios.get(URI + ENDPOINT);
+        console.log(response);
+        response.data.map((country) => {
+            const item = document.createElement('li');
+            item.setAttribute('class', 'africa');
+            item.textContent = country.data.name;
+
+            // toevoegen aan de list
+            list.appendChild( country );
+        })
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+getCountryData();
