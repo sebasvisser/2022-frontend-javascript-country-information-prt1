a0927109925ca259a24cbe7e8932a2291f489484
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-09 11:00:05 +0100

    opdracht gemaakt

--- src/app.js
+++ src/app.js
@@ -4,23 +4,61 @@ import axios
 const list = document.getElementById("country-list");
 
 async function getCountryData() {
-    const URI =" https://restcountries.com/";
-    const ENDPOINT = "v3.1/all" ;
     try{
-        const response = await axios.get(URI + ENDPOINT);
-        console.log(response);
-        response.data.map((country) => {
-            const item = document.createElement('li');
-            item.setAttribute('class', 'africa');
-            item.textContent = country.data.name;
+// aanroepen van de data
+        const response = await axios.get("https://restcountries.com/v2/all");
+        const countries = response.data;
+// op volgorde zetten
+        countries.sort((a, b) => {
+            return a.population - b.population;
+        });
 
-            // toevoegen aan de list
-            list.appendChild( country );
+// Met map functie over alle items in de lijst gaan.
+        countries.map((country) => {
+        // li als container voor blokje per land
+            const item = document.createElement('li');
+            const countryFlag = document.createElement("img");
+            const countryName = document.createElement("span");
+            const countryPopulation = document.createElement("p");
+
+            // Vlag
+            countryFlag.setAttribute("alt", country.name + "_flag");
+            countryFlag.setAttribute("src", country.flag);
+            countryFlag.setAttribute("class", "flag");
+            // Naam van Land
+            countryName.setAttribute("class", getRegionClass(country.region));
+            countryName.textContent = country.name;
+            // Populatie zinnetje
+            countryPopulation.setAttribute("class", getRegionClass(country.region));
+            countryPopulation.textContent = "Has a population of " + country.population + " people";
+
+// toevoegen aan de list
+            list.append(item);
+            item.append(countryFlag);
+            item.append(countryName);
+            item.append(countryPopulation);
         })
 
     }
     catch (error) {
         console.error(error);
+    }
+}
+
+function getRegionClass(currentRegion) {
+    switch (currentRegion) {
+        case 'Africa':
+            return 'africa';
+        case 'Americas':
+            return 'americas';
+        case 'Asia':
+            return 'asia';
+        case 'Europe':
+            return 'europe';
+        case 'Oceania':
+            return 'oceania';
+        default:
+            return 'default';
     }
 }
 
